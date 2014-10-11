daf302c9a6cffc9512e19efbb72b1de53700da9e
henryyou <henryyou@fb.com>
2014-10-10 17:10:43 -0700

    tipcalculator project submission
    Signed-off-by: henryyou <henryyou@fb.com>

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Jin You on 10/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Jin You on 10/8/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
